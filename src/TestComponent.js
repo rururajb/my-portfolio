f9ce7587cc4d2207a87bea6b1484c20c9db62510
rururajb <148726448+rururajb@users.noreply.github.com>
2025-03-10 22:23:05 -0700

    Might have to make another website

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -1,7 +1,7 @@
-// src/App.js
+// src/TestComponent.js
 import React from 'react';
 
-function App() {
+function TestComponent() {
   return (
     <div className="min-h-screen bg-red-500 text-white flex items-center justify-center">
       <h1 className="text-5xl">Red Screen Test</h1>
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+export default TestComponent;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        pastelPurple: '#d8b4fe', // This is Tailwind’s purple-400
-        pastelBlue: '#bae6fd'   // This is Tailwind’s sky-300
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
