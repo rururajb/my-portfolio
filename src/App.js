26a061e051556244222b1ad2a4ecb653059463ae
rururajb <148726448+rururajb@users.noreply.github.com>
2025-03-21 20:33:17 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,2 @@
 // src/App.js
-import React from 'react';
 
-function App() {
-  return (
-    <div className="min-he flex items-center justify-center">
-      <h1 className="text-5xl">Red Screen Test</h1>
-    </div>
-  );
-}
-
-export default App;
